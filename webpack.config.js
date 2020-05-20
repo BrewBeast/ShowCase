4870014edf5fc51e71d4a2f818acb22910c8d3e2
Matthias Fore <matthiasfore@gmail.com>
2020-05-20 16:59:25 -0700

    webpack and react live

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/public');
 
 module.exports = {
-  mode: 'developement',
+  mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
@@ -15,7 +15,7 @@ module.exports = {
       {
         test: /\.jsx?/,
         include: SRC_DIR,
-        expclude: /node_modules/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
